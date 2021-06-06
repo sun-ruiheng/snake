2ec835554d7232b946c971b94da7809927af34bb
Sun Ruiheng <sunruiheng2001@gmail.com>
2021-06-06 16:47:55 +0800

    basic features

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("Snake")
+icon = pygame.image.load('snake.png')
+pygame.display.set_icon(icon)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 85))
+    pygame.display.update()
