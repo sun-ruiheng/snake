32cc5ec48a6d709f0c831386a2dab35c4acd52d7
Sun Ruiheng <sunruiheng2001@gmail.com>
2021-06-06 19:10:05 +0800

    made a snake

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
+from snake import *
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+bounds = (800, 500)
+screen = pygame.display.set_mode(bounds)
+
+block_size = 20
+snake = Snake(block_size, bounds)
 
 pygame.display.set_caption("Snake")
 icon = pygame.image.load('snake.png')
@@ -9,9 +14,23 @@ pygame.display.set_icon(icon)
 
 running = True
 while running:
+    pygame.time.delay(100)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 85))
-    pygame.display.update()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        snake.steer(Direction.UP)
+    elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        snake.steer(Direction.DOWN)
+    elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        snake.steer(Direction.LEFT)
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        snake.steer(Direction.RIGHT)
+    snake.move()
+
+    screen.fill((0, 0, 80))
+    snake.draw(pygame, screen)
+    pygame.display.update() #or flip()
---
+++ snake.py
@@ -0,0 +1,58 @@
+from enum import Enum
+
+
+class Direction(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+
+class Snake():
+    length = None
+    direction = None
+    body = None
+    block_size = None
+    color = (230, 230, 250)
+    bounds = None
+
+    def __init__(self, block_size, bounds):
+        self.block_size = block_size
+        self.bounds = bounds
+        self.respawn()
+
+    def respawn(self):
+        self.length = 3
+        self.body = [(20, 20), (20, 40), (20, 60)]
+        self.direction = Direction.DOWN
+
+    def draw(self, game, window):
+        for block in self.body:
+            game.draw.rect(window, self.color, (block[0], block[1], self.block_size, self.block_size))
+
+    def move(self):
+        cur_head = self.body[-1]
+        if self.direction == Direction.UP:
+            new_head = (cur_head[0], cur_head[1] - 1)
+            self.body.append(new_head)
+        elif self.direction == Direction.DOWN:
+            new_head = (cur_head[0], cur_head[1] + 1)
+            self.body.append(new_head)
+        elif self.direction == Direction.LEFT:
+            new_head = (cur_head[0] - 1, cur_head[1])
+            self.body.append(new_head)
+        elif self.direction == Direction.RIGHT:
+            new_head = (cur_head[0] + 1, cur_head[1])
+            self.body.append(new_head)
+        if self.length < len(self.body):
+            self.body.pop(0)
+
+    def steer(self, direction):
+        if direction == Direction.UP and self.direction != Direction.DOWN:
+            self.direction = direction
+        elif direction == Direction.DOWN and self.direction != Direction.UP:
+            self.direction = direction
+        elif direction == Direction.LEFT and self.direction != Direction.RIGHT:
+            self.direction = direction
+        elif direction == Direction.RIGHT and self.direction != Direction.LEFT:
+            self.direction = direction
